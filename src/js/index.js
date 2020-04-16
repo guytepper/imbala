8dd4225d9673703bdc71fefc88377b8c810a1176
Guy Tepper <guytepper@gmail.com>
2020-04-16 18:27:36 +0300

    Added mobile navigation (#1)

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,16 @@ const menuBtn = document.querySelector('.menu-btn');
 const siteNav = document.querySelector('.site-nav');
 const siteNavMenu = document.querySelector('.site-nav-menu');
 const navClosebtn = document.querySelector('.nav-close-button');
+const fadedBackground = document.querySelector('.faded-background');
 
 function toggleMenu() {
-	siteNav.classList.toggle('menu-active');	
+  siteNav.classList.toggle('menu-active');
 }
 
 menuBtn.onclick = toggleMenu;
 navClosebtn.onclick = toggleMenu;
+fadedBackground.onclick = toggleMenu;
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('body').classList.toggle('no-js');
+});
