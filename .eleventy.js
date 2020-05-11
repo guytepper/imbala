4553f4f0d998935648ad10381282268fa40f94ce
Guy Tepper <guytepper@gmail.com>
2020-05-11 13:21:43 +0300

    Added build script
    + Removed css files copy, as the new main.css file already include them

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/js': 'js' });
-  eleventyConfig.addPassthroughCopy({ 'src/css': 'css' });
   eleventyConfig.addPassthroughCopy({ 'src/images': 'images' });
 
   return {
