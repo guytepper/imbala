007095aaba59506e9ed74c1b20e4dc07e280f768
Guy Tepper <guytepper@gmail.com>
2020-06-28 08:25:52 +0300

    Modifiy events config

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/js': 'js' });
   eleventyConfig.addPassthroughCopy({ 'src/css/fonts': 'css/fonts' });
   eleventyConfig.addPassthroughCopy({ 'src/images': 'images' });
+  eleventyConfig.addPassthroughCopy({ 'src/admin': 'admin' });
 
   eleventyConfig.addCollection('events', function (collection) {
     let allEvents = collection.getFilteredByGlob('**/*.md');
